470805599f286426ec41347fb53c7ea957f0d5a7
BK <bkarthikeyan@skava.com>
2020-12-29 14:10:30 +0530

    code updated

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "79e8a8ff01e5c6930b09";
+/******/ 	var hotCurrentHash = "b0733737dac2d825a7aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
